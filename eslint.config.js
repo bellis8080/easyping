0e785adc7d9e14145bb18f4d2f51b53dcf8f6094
Brad Ellis <bellis8080@gmail.com>
2025-10-22 16:51:44 -0500

    test: verify pre-commit hook with lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+const js = require('@eslint/js');
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'prefer-const': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.next/',
+      'out/',
+      'build/',
+      'coverage/',
+      '.turbo/',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  },
+  prettier,
+];
